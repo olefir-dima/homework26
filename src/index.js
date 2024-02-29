813340954241f02cf8efd53704787e889495ea2a
Dmitry <dmitry.olefir@novidea.com>
2024-02-29 19:07:24 +0200

    hw26

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import "./style.css";
+
+document.getElementById("magic-ball").addEventListener("click", () => {
+  const responses = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes, definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy, try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful",
+  ];
+  const randomIndex = Math.floor(Math.random() * responses.length);
+  const answer = responses[randomIndex];
+  document.getElementById("magic-ball").textContent = answer;
+});
